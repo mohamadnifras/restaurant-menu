e564f2aeba704824a7bdae0a67ef5865c03ce8d1
mohamadnifras <mohamadnifras2004@gmail.com>
2025-08-22 17:08:03 +0530

    hvjhv

--- frontend/src/api/axioseInstance.ts
+++ frontend/src/api/axioseInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACK_END_URL,
+    baseURL: "https://restaurant-menu-3adz.onrender.com/api",
     withCredentials: true,
 })
 
